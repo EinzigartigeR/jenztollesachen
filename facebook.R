c5bca18ade4360ca874708c89b5b6fb899a20e8d
Jens Bruno Wittek <wittek@uni-bielefeld.de>
2015-02-21 01:40:43 +0100

    Update facebook.R
    better init (complete df, thus less variables and storage needed)

--- facebook.R
+++ facebook.R
@@ -6,7 +6,7 @@
 # required package
 library(Rfacebook)
 #!!! Fb token, needs to be generated at https://developers.facebook.com/tools/explorer (attention: will expire soon)
-fbtoken = "CAACEdEose0cBAKRZAZAIT5BRvo6ztniFaRlCf5K8dFLNOuHpK09ZAosMFXmmApMmtrIclWezvTGnxcEh9573PZASOKLZA8UU2ZAcYIlzCXJHAAkh56SAzKpv3N7OQZAZCHsNNPrvhre3DTXD8vgG696WA4ePspqkD92oFqTSGl7blL1HggYwO9mCH5umZBtyK2Ko5v0ZBJ3kHEGrY01S0Iv2OZCdPs42F3pzPYZD"
+fbtoken = "CAACEdEose0cBAGGBkRQw1ZCqHiAjRICAekwGk8vmBPkhsg4YNlPKWFc8IgNcFdga3KyDDTMkh5OSiO5bqGWBj2q9NQBNHSahzEyNGMAkZAFWTivJQlNcuKp61wSKdLCszwY4daoZCNmWAo0FnpImEb7otFNgZAnCTKf9bumOSMaW05lxNYH2Ex3ZACbBMYMRdgs1eVfKOhMj9FzLxA7c2N3uZBrVBdN6IZD"
 #!!! Fb page, such as https://www.facebook.com/rbloggers
 fbname = "rbloggers"
 #!!! count of posts you want to scrape
@@ -14,8 +14,8 @@ count = 10
 # access Fb page
 fbpage = getPage(page=fbname,token=fbtoken,n=count)
 # init
-likescount = commentscount = sharescount = integer(count)
-createdtime = posttype = postcontent = character(count)
+output = data.frame(matrix(ncol=6,nrow=count))
+names(output) = c("Likes","Comments","Shares","Time","Type","Content")
 # function for waiting
 wait = function(x) {
   runtime = proc.time()
@@ -25,16 +25,17 @@ wait = function(x) {
 # access data
 for (i in 1:count) {
   x = getPost(post=fbpage$id[i],n=1,token=fbtoken)
-  likescount[i] = x$post$likes_count
-  commentscount[i] = x$post$comments_count
-  sharescount[i] = x$post$shares_count
-  createdtime[i] = x$post$created_time
-  posttype[i] = x$post$type
-  postcontent[i] = x$post$message
+  output$Likes[i] = x$post$likes_count
+  output$Comments[i] = x$post$comments_count
+  output$Shares[i] = x$post$shares_count
+  output$Time[i] = x$post$created_time
+  output$Type[i] = x$post$type
+  output$Content[i] = x$post$message
   # wait 5 seconds until next query (otherwise the Fb limit will be reached) - function see above
   wait(5)
 }
-# save data()(^\\n$)()
-output = data.frame(Likes=likescount,Comments=commentscount,Shares=sharescount,Time=createdtime,Type=posttype,Content=postcontent)
-output$Content = sub(pattern="\n",replacement="; ",output$Content,fixed=TRUE)
-write.table(output,"M://R/Facebook.csv",sep=";",eol="\r",row.names=FALSE,na="")
+# save data
+output$Content = gsub(pattern="\n",replacement="; ",output$Content,fixed=TRUE)
+write.table(output,"M://Github/Facebook.csv",sep=";",eol="\r",row.names=FALSE,na="")
+
+
